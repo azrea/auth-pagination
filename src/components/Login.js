77c5de17fd6b869cd20aa361ccb56bc0f35e4a05
David Attah <dattah524@gmail.com>
2022-02-23 11:09:53 +0000

    added functionality to form and dashboard. operates pretty smoothly

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { FormContainer } from "./StyledComponents";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const res = await fetch("http://localhost:5000/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user),
+      });
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const { email, password } = user;
   return (
     <FormContainer>
       <h1>Login</h1>
-      <form>
-        <input type="email" placeholder="What is your Email?" />
-        <input type="password" placeholder="Enter your Password please" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="What is your Email?"
+          value={email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+        <input
+          type="password"
+          placeholder="Enter your Password please"
+          value={password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+
         <button className="submitBtn" type="submit">
           Submit
         </button>
-        <span>
-          Don't have an account?
-          <button className="registerBtn">Register</button>
-        </span>
       </form>
+      <span>
+        Don't have an account?
+        <button className="registerBtn">
+          <Link to="/register">Register</Link>
+        </button>
+      </span>
     </FormContainer>
   );
 };
