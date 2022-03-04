3e4602edf5d6810e2f6b4f004200a2397766cc4b
David Attah <dattah524@gmail.com>
2022-03-04 14:20:06 +0000

    created hero and its template for the multiple slides

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Hero from "../components/Hero";
 import NavBar from "../components/NavBar";
 import { StyledHomePage } from "../components/StyledComponents";
 
@@ -7,18 +8,7 @@ const Homepage = () => {
     <StyledHomePage>
       <NavBar />
       {/* 3 heroes with themes e.g movie pictures and games */}
-
-      <div className="hero">
-        <div className="flexHero">
-          <span>
-            <h2>Experience the Joy of gazing at the</h2>
-            <h1>Night Sky Collection</h1>
-            <h1>Named</h1>
-            <h2>Only Price</h2>
-          </span>
-          <img src="" alt="Sample here" />
-        </div>
-      </div>
+      <Hero />
     </StyledHomePage>
   );
 };
